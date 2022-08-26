1597b22785c88b8bfcb47fd3edf8bc132669c6b9
Mohamad Yehya <mohamad.yehya.shahbur@gmail.com>
2022-08-26 23:08:22 +0300

    add article_list function

--- base/views.py
+++ base/views.py
@@ -7,3 +7,20 @@ from rest_framework.parsers import JSONParser
 from base.models import Article
 from base.serializer import ArticleSerializer
 
+# Create your views here.
+
+
+@csrf_exempt
+def article_list(request):
+    if request.method == 'GET':
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = ArticleSerializer(data = data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data ,status = 201)
+        return JsonResponse(serializer.errors, status= 400)
